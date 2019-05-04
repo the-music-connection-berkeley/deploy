a743be1de575810213b5a00be300db4f1a3e8717
Martin Kim <minty0829@berkeley.edu>
2019-05-03 17:56:25 -0700

    Font changes and scroll

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -4,7 +4,7 @@ window.onload = init;
 function init() {
   var scroll_btn = document.getElementById("scroll_btn");
   scroll_btn.addEventListener('click', function(){
-    var elem = document.querySelector(".pacifico");
+    var elem = document.querySelector(".row");
     elem.scrollIntoView({
   behavior: 'smooth'
     });
