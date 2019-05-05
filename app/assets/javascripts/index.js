8ed53698fc1a347ea97b4d7749506f7d2800f84e
Martin Kim <minty0829@berkeley.edu>
2019-05-05 15:11:18 -0700

    Almost done with typewriting effec

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,7 +1,30 @@
 
 window.onload = init;
+var index = 0;
+var i = 0;
 
 function init() {
+  var text = ["Connecting People", "Through Music"];
+  var typeInterval = setInterval(typeWriter, 200);
+  var element = document.querySelector(".motto");
+
+  function typeWriter() {
+    var elem = element.children[index];
+    if (elem) {
+      elem.innerHTML = elem.innerHTML + text[index].charAt(i);
+      i++;
+      if (i == text[index].length) {
+        i = 0;
+        element.children[index].classList.remove("typewriter");
+        index = index + 1;
+        element.children[index].classList.add("typewriter");
+      }
+    } else {
+      clearInterval(typeInterval);
+    }
+  }
+
+
   var scroll_btn = document.getElementById("scroll_btn");
   scroll_btn.addEventListener('click', function(){
     var elem = document.querySelector(".row");
