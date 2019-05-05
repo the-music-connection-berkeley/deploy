b3db0c33d0c3cb0aab2e3321aae41a6af79df028
Matthew Lam <>
2019-05-05 16:14:14 -0700

    fixed test

--- spec/factories/parent_factory.rb
+++ spec/factories/parent_factory.rb
@@ -5,7 +5,8 @@ FactoryGirl.define do
     email 'musicconnection@gmail.com'
     address "Deadman's Lane"
     grade '9'
-    piano 'Yes'
+    piano_home 'Yes'
+    time_availability "3-5"
     instruments 'Piano'
     experiences '6 months'
     pastapp 'Yes'
@@ -14,3 +15,16 @@ FactoryGirl.define do
     updated_at 'false'
   end
 end
+t.string   "name"
+t.string   "phone"
+t.string   "email"
+t.string   "address"
+t.string   "grade"
+t.string   "piano_home"
+t.string   "time_availability"
+t.string   "instruments"
+t.string   "experiences"
+t.string   "pastapp"
+t.string   "lunch"
+t.datetime "created_at",        null: false
+t.datetime "updated_at",
