3b067b33cdc09dabe9910b0c1291fbdfa6248524
Sung Chang <steadyaction@gmail.com>
2019-05-05 16:01:13 -0700

    .

---
+++ public/assets/index-0f2ac73ee3adb0aae668dd99cbb1a98baf8271745789bdf04e01f35ffff6206e.js
@@ -0,0 +1,13 @@
+
+window.onload = init;
+
+function init() {
+  var scroll_btn = document.getElementById("scroll_btn");
+  scroll_btn.addEventListener('click', function(){
+    var elem = document.querySelector(".row");
+    elem.scrollIntoView({
+  behavior: 'smooth'
+    });
+  });
+}
+;
