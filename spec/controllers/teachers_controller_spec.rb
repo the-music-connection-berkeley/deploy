4f5502835d42a8e3c36d8de641cd9e838ac30db5
Matthew Lam <>
2019-05-06 00:15:42 -0700

    added some more test cases

--- spec/controllers/teachers_controller_spec.rb
+++ spec/controllers/teachers_controller_spec.rb
@@ -8,4 +8,27 @@ describe TeachersController do
       expect(teacher.name).to eql(Teacher.first.name)
     end
   end
+  describe 'POST create' do
+
+    it 'should create new teacher' do
+      post :create, :params => {:name => 'No name',
+      :phone => '818-348-3833',
+      email: 'musicconnection@gmail.com',
+      class_name: 'best class',
+      school_name: 'best school',
+      grade: '9',
+      time_availability: "3-5",
+      instruments: 'Piano',
+      comment: 'no comment'}
+      expect(Teacher.count).to eql (1)
+    end
+end
+describe 'DELETE ' do
+  let!(:teacher1) {FactoryGirl.create(:teacher)}
+
+  it 'destroys a teacher' do
+    expect { delete :destroy, id: teacher1.id
+    }.to change(Teacher, :count).by(-1)
+  end
+end
 end
--- spec/controllers/tutors_controller_spec.rb
+++ spec/controllers/tutors_controller_spec.rb
@@ -8,4 +8,34 @@ describe TutorsController do
       expect(tutor.name).to eql(Tutor.first.name)
     end
   end
+  describe 'POST create' do
+    it 'should create new tutor' do
+      post :create, :params => {name: 'No name',
+      phone: '818-348-3833',
+      sid: '303032009',
+      email: 'musicconnection@gmail.com',
+      year: '9',
+      major: 'CS',
+      minor: 'CS',
+      experiences: '6 months',
+      time_availability: "3-5",
+      preferred_grade: '7~9',
+      instruments: 'Piano',
+      private: 'Yes',
+      piano_vocal: 'Piano',
+      returning: 'Yes',
+      prev_again: 'Yes',
+      preffered_student_class: "none",
+      comment: 'no comment'}
+      expect(Tutor.count).to eql (1)
+    end
+end
+describe 'DELETE ' do
+  let!(:tutor1) {FactoryGirl.create(:tutor)}
+
+  it 'destroys a tutor' do
+    expect { delete :destroy, id: tutor1.id
+    }.to change(Tutor, :count).by(-1)
+  end
+end
 end
