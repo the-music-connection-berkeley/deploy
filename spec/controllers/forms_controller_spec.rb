1a00665b70be53f5b6a859ebea1917d14fe15abf
Matthew Lam <matthewlam@berkeley.edu>
2019-05-07 16:48:47 -0700

    updated test coverage to fix travis build

--- spec/controllers/forms_controller_spec.rb
+++ spec/controllers/forms_controller_spec.rb
@@ -28,21 +28,21 @@ describe FormsController do
     it "successfully submits" do
       post :teacher_submit, :question => {:teacher_name => "My name", :phone => "818",
       :weekday => ["Monday", "Tuesday"], :start_time => ["2", "3"], :end_time => ["4", "6"],
-      :instrument => ["Oboe"]}
+      :instrument => ["Oboe", "Others"], :others => ["Violin", "Trumpet"]}
     end
   end
   describe "POST tutor_submit" do
     it "successfully submits" do
       post :tutor_submit, :question => {:name => "My name", :phone => "818",
       :weekday => ["Monday", "Tuesday"], :start_time => ["2", "3"], :end_time => ["4", "6"],
-      :instrument => ["Oboe"]}
+      :instrument => ["Oboe", "Others"], :others => ["Violin", "Trumpet"], :piano_vocal => "Piano"}
     end
   end
   describe "POST parent_submit" do
     it "successfully submits" do
       post :parent_submit, :question => {:name => "My name", :phone => "818",
       :weekday => ["Monday", "Tuesday"], :start_time => ["2", "3"], :end_time => ["4", "6"],
-      :instrument => ["Oboe"]}
+      :instrument => ["Oboe", "Others"], :others => ["Violin", "Trumpet"]}
     end
   end
 end
