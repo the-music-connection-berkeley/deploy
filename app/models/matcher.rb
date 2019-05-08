ba81abd64e22f9974976dccba178e4611f25ad54
Hong Seok Jang <hongseok219@gmail.com>
2019-05-07 17:57:47 -0700

    Merge matcher and algo branch

--- app/models/matcher.rb
+++ app/models/matcher.rb
@@ -43,37 +43,37 @@ class Matcher
     end
 
     # ["1&2&3"]
-    def get_times(person)
-        ret = []
-        person['availabilities'].split('&').each do |tid|
-            ret.append(Availability.find(id=tid))
-        end
-    end
+ def get_times(person)
+     ret = []
+     person['availabilities'].split('&').each do |tid|
+         ret.append(Availability.find(id=tid))
+     end
+ end
 
-    def time_matches?(tutor, tutee)
-        get_day = lambda {|t| t['weekday']}
-        get_start = lambda {|t| t['start_time']}
-        get_end = lambda {|t| t['end_time']}
-        i = j = 0
+ def time_matches?(tutor, tutee)
+     get_day = lambda {|t| t['weekday']}
+     get_start = lambda {|t| t['start_time'].split(':')}
+     get_end = lambda {|t| t['end_time'].split(':')}
+     i = j = 0
 
-        tutor_times = get_times(tutor) # [{'weekday': 'Tuesday', 'start_time': '6', 'end_time': '9'}]
-        tutee_times = get_times(tutee)
+     tutor_times = get_times(tutor) # [{'weekday': 'Tuesday', 'start_time': '6', 'end_time': '9'}]
+     tutee_times = get_times(tutee)
 
-        tutor_times.each do |t1|
-            tutee_times.each do |t2|
-                if get_day(t1) == get_day(t2)
-                    start_t = [get_start(t1), get_start(t2)].max
-                    end_t = [get_end(t1), get_end(t2)].min
-                    if end-start >= 60
-                        if tutor['id'] not in self['matched']_records
-                            self['matched']_records[tutor['id']] = []
-                        end
-                        self['matched']_records[tutor['id']] << tutee['id']
-                    end
-                end
-            end
-        end
-    end
+     tutor_times.each do |t1|
+         tutee_times.each do |t2|
+             if get_day(t1) == get_day(t2)
+                 start_t = [get_start(t1), get_start(t2)].max
+                 end_t = [get_end(t1), get_end(t2)].min
+                 if (end[0].to_i*60 + end[1].to_i) - (start[0].to_i*60 + start[1].to_i) >= 60
+                     if !(tutor['id'] in self.matched_records)
+                         self.matched_records[tutor['id']] = []
+                     end
+                     self.matched_records[tutor['id']] << tutee['id']
+                 end
+             end
+         end
+     end
+ end
 
     def sort
        sorted_tutors.sort_by{ |tutor| tutor['number_of_matches'] }
