1b07bb1a47629b83904f253884572759e44dfcbb
Hong Seok Jang <hongseok219@gmail.com>
2019-05-07 17:36:49 -0700

    finish algo without testing

--- app/models/matcher.rb
+++ app/models/matcher.rb
@@ -2,35 +2,49 @@ class Matcher
     def initialize
     end
 
-    def match
+    def split_helper
+
+    def match_counter
         teachers = Teacher.all
         tutors = Tutor.all
         parents = Parent.all
 
         tutors.each do |tutor|
           if tutor.private == "yes":
-            if tutor.piano_vocal == "vocal":
-              match(tutor, parents, "vocal")
-            elsif tutor.piano_vocal == "piano":
-              match(tutor, parents, "piano")
-            end
+            parent_counter(tutor)
           end
           if tutor.in_class = "yes":
-            tutor.instruments.each do |instrument|
-              match(tutor, teachers, instrument)
-            end
+            teacher_counter(tutor)
           end
-      end
-
-    def match(tutor, tutees, instrument)
-      tutees.each do |tutee|
-        if instrument in tutee.instruments && time_matches?(tutor, tutee)
-            tutor.number_of_matches += 1
         end
     end
 
-    "[[\"Wednesday\", [\"13:30\", \"14:00\"]], [\"Tuesday\", [\"13:00\", \"14:30\"]]]"
-    def time_matches?(tutor_time, teacher_time)
+    def parent_counter(tutor)
+      Parents.all.each do |parent|
+        if tutor.piano_vocal == parent.instruments
+          tutor.number_of_matches += 1
+          parent.number_of_matches += 1
+        end
+      end
+
+    def teacher_counter(tutor)
+      instruments = tutor.instruments.split("&")
+      Teacher.all.each do |teacher|
+        instruments.each do |instrument|
+          if instrument in teacher.instruments
+            if time_matches?(tutor, teacher)
+              tutor.number_of_matches += 1
+              teacher.number_of_matches += 1
+              break
+            end
+          end
+        end
+      end
+    end
+
+
+    //"1&2" -> parsing -> availabilities.find(1,2)
+    def time_matches?(tutor, teacher)
         tutor_time.each do |time_block1|
             day1 = time_block1[0]
             range1 = Date.new(time_block1[1])
@@ -58,6 +72,7 @@ class Matcher
             end
         end
     end
+
 
 
     def match_parent_tutor
