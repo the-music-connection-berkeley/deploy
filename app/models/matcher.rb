d4cbdad2504ec304e240670c9310706b42dd9d36
Hong Seok Jang <hongseok219@gmail.com>
2019-05-07 18:15:34 -0700

    add comment

--- app/models/matcher.rb
+++ app/models/matcher.rb
@@ -1,5 +1,7 @@
 class Matcher
+    #match_counter -> sort -> final_match
 
+    #key = tutor_id, value = [possible matched parents or teachers]
     possible_matches = Hash.new([])
     sorted_tutors = Tutors.all
 
@@ -18,6 +20,7 @@ class Matcher
         end
     end
 
+    #it saves the #of matches and updates hashmap
     def parent_counter(tutor)
       Parents.all.each do |parent|
         if tutor.piano_vocal == parent.instrument[0] && time_matches?(tutor, parent)
@@ -28,6 +31,7 @@ class Matcher
       end
     end
 
+    #it saves the #of matches and updates hashmap
     def teacher_counter(tutor)
       instruments = tutor.instruments.split("&")
       Teacher.all.each do |teacher|
@@ -76,15 +80,20 @@ class Matcher
  end
 
     def sort
+      # Sort tutors table by # of matches
        sorted_tutors.sort_by{ |tutor| tutor['number_of_matches'] }
+       # Sort the possible matches by ascending number of matches for each tutor
        possible_matches.each do |tutor_id|
          possible_matches[tutor_id].sort_by{ |tutee| tutee['number_of_matches'] }
        end
     end
 
     def final_match
+      # for each tutor, (sorted in order from least to most number of possible matches)
       sorted_tutors.each do |sorted_tutor|
+        # we go through its possible matches (also sorted in order from least to most number of possible matches)
         possible_matches[sorted_tutor['id']].each do |tutee|
+          # if the tutee is matched then we skip over it, otherwise we match it with the tutor
           if tutee['matched'] == 0
             tutee['matched'] = 1
             tutor['matched'] = 1
