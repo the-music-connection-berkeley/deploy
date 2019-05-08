35ff578bc4a8c926c98f2efdfcdf37e2095ad26b
Matthew Lam <matthewlam@berkeley.edu>
2019-05-07 17:13:58 -0700

    cleaned up files + cleaned up admin page a bit

--- app/controllers/tutors_controller.rb
+++ app/controllers/tutors_controller.rb
@@ -23,52 +23,5 @@ class TutorsController < ApplicationController
 
   # POST /tutors
   # POST /tutors.json
-  def create
-    @tutor = Tutor.new(tutor_params)
 
-    respond_to do |format|
-      if @tutor.save
-        format.html { redirect_to @tutor, notice: 'Tutor was successfully created.' }
-        format.json { render :show, status: :created, location: @tutor }
-      else
-        format.html { render :new }
-        format.json { render json: @tutor.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /tutors/1
-  # PATCH/PUT /tutors/1.json
-  def update
-    respond_to do |format|
-      if @tutor.update(tutor_params)
-        format.html { redirect_to @tutor, notice: 'Tutor was successfully updated.' }
-        format.json { render :show, status: :ok, location: @tutor }
-      else
-        format.html { render :edit }
-        format.json { render json: @tutor.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /tutors/1
-  # DELETE /tutors/1.json
-  def destroy
-    @tutor.destroy
-    respond_to do |format|
-      format.html { redirect_to tutors_url, notice: 'Tutor was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_tutor
-      @tutor = Tutor.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def tutor_params
-      params.fetch(:tutor, {})
-    end
 end
