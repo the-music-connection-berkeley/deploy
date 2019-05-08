78ae0dc58b335a22c60bde092fb45071a62e9a01
Sung Chang <steadyaction@gmail.com>
2019-05-07 22:06:48 -0700

    WORKING ALGO !!!!!!!!!!!!!!!!!!!!!!

--- app/models/matcher.rb
+++ app/models/matcher.rb
@@ -1,8 +1,8 @@
 class Matcher
     #match_counter -> sort -> final_match
     #key = tutor_id, value = [possible matched parents or teachers]
-    @possible_matches = {}
-    @sorted_tutors = Tutor.all
+    @possible_matches
+    @sorted_tutors
 
     def initialize
         @possible_matches = {}
@@ -12,6 +12,8 @@ class Matcher
     def possible_matches
         @possible_matches
     end
+
+
     def main
         match_counter
         sort
@@ -25,10 +27,9 @@ class Matcher
         parents = Parent.all
 
         tutors.each do |tutor|
-            if tutor.private == "yes"
+            if tutor['private'] == "yes"
                 parent_counter(tutor)
-            end
-            if tutor.in_class == "yes"
+            else
                 teacher_counter(tutor)
             end
         end
@@ -36,25 +37,28 @@ class Matcher
 
     #it saves the #of matches and updates hashmap
     def parent_counter(tutor)
-        Parents.all.each do |parent|
+        Parent.all.each do |parent|
             if tutor.piano_vocal == parent.instrument[0] && time_matches?(tutor, parent)
                 tutor['number_of_matches'] += 1
                 parent['number_of_matches'] += 1
-                possible_matches[tutor['id']] += [parent]
+                @possible_matches[tutor['id']] += [parent]
             end
         end
     end
 
     #it saves the #of matches and updates hashmap
     def teacher_counter(tutor)
-        instruments = tutor.instruments.split("&")
+        instruments = tutor.instrument.split("&")
         Teacher.all.each do |teacher|
             instruments.each do |instrument|
-                if teacher.instruments.includes? instrument && time_matches?(tutor, teacher)
+                if teacher.instrument.include? instrument and time_matches?(tutor, teacher)
                     tutor['number_of_matches'] += 1
                     teacher['number_of_matches'] += 1
-                    possible_matches[tutor['id']] += [teacher]
-                    break
+                    if @possible_matches.key? tutor['id'] 
+                        @possible_matches[tutor['id']] += [teacher]
+                    else
+                        @possible_matches[tutor['id']] = [teacher]
+                    end 
                 end
             end
         end
@@ -69,6 +73,7 @@ class Matcher
         return ret
     end
 
+    # it's working dont change
     def time_matches?(tutor, tutee)
         i = j = 0
         possible = false
@@ -85,10 +90,6 @@ class Matcher
                     end_t[0] = end_t[0].split(":")
                     end_t[1] = end_t[1].split(":")
                     if (end_t[0][0].to_i * 60 + end_t[0][1].to_i) - (start_t[1][0].to_i * 60 + start_t[1][1].to_i) >= 60
-                        if !(@possible_matches.key?(tutor['id']))
-                            @possible_matches[tutor['id']] = []
-                        end
-                        @possible_matches[tutor['id']] << tutee['id']
                         possible = true
                     end
                 end
@@ -99,24 +100,34 @@ class Matcher
 
     def sort
         # Sort tutors table by # of matches
-        sorted_tutors.sort_by{ |tutor| tutor['number_of_matches'] }
+        @sorted_tutors.sort_by{ |tutor| tutor['number_of_matches'] }
         # Sort the possible matches by ascending number of matches for each tutor
-        possible_matches.each do |tutor_id|
-            possible_matches[tutor_id].sort_by{ |tutee| tutee['number_of_matches'] }
+        @possible_matches.keys.each do |tutor_id|
+            @possible_matches[tutor_id].sort_by{ |tutee| tutee['number_of_matches'] }
         end
     end
 
     def final_match
         # for each tutor, (sorted in order from least to most number of possible matches)
-        sorted_tutors.each do |sorted_tutor|
+        @sorted_tutors.each do |sorted_tutor|
         # we go through its possible matches (also sorted in order from least to most number of possible matches)
-            possible_matches[sorted_tutor['id']].each do |tutee|
-                # if the tutee is matched then we skip over it, otherwise we match it with the tutor
-                if tutee['matched'] == 0
-                    tutee['matched'] = 1
-                    tutor['matched'] = 1
-                    Matched.tutor = tutor
-                    Matched.teacher = tutor
+            if @possible_matches[sorted_tutor['id']].nil? 
+                puts "no"
+            else
+                puts "yes"
+                @possible_matches[sorted_tutor['id']].each do |tutee|
+                    # if the tutee is matched then we skip over it, otherwise we match it with the tutor
+                    if tutee['matched'] == false 
+                        tutee['matched'] = true
+                        sorted_tutor['matched'] = true
+                        client_type = ""
+                        if tutee.class == "Parent"
+                            client_type = "p"
+                        else
+                            client_type = "t"
+                        end
+                        Match.create(:tutor_id => sorted_tutor['id'], :tutee_id => client_type + tutee['id'].to_s)
+                    end
                 end
             end
         end
