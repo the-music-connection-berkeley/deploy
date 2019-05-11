fc06be139b0f68e1284751b466597a7286a817cd
Eugene Lee <eugenelee24@berkeley.edu>
2019-05-11 15:41:45 -0700

    Create login

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,8 @@ Rails.application.routes.draw do
 
   get 'forms/tutor'
 
+  get 'admin/welcome'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   # You can have the root of your site routed with "root"
